00d4dc1db1c20b839e2d49186ac1b27256d08887
terklin <brendanpjohnson@gmail.com>
2025-06-26 20:21:36 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,45 @@
-// Initialize the map
-var map = L.map('map', {
+
+// Initialize the map with CRS.Simple for image coordinates
+const map = L.map('map', {
     crs: L.CRS.Simple,
     minZoom: -2
 });
 
-// Dimensions of the fantasy city image
-var imageWidth = 2000;
-var imageHeight = 1500;
-var bounds = [[0,0], [imageHeight, imageWidth]];
+// Dimensions of the image
+const imageWidth = 4096;
+const imageHeight = 3072;
+const bounds = [[0, 0], [imageHeight, imageWidth]];
 
-// Add the fantasy city image as an overlay
-var image = L.imageOverlay('City_of_Orbonne.jpg', bounds).addTo(map);
+// Add the image overlay
+L.imageOverlay('City_of_Orbonne.jpg', bounds).addTo(map);
+
+// Set the view to the center of the image
 map.fitBounds(bounds);
 
-// Function to create a popup form
-function createPopupForm(latlng) {
-    var container = L.DomUtil.create('div');
-    var inputTitle = L.DomUtil.create('input', '', container);
-    inputTitle.type = 'text';
-    inputTitle.placeholder = 'Title';
+// Load saved markers from localStorage
+const savedMarkers = JSON.parse(localStorage.getItem('markers')) || [];
 
-    var inputDesc = L.DomUtil.create('textarea', '', container);
-    inputDesc.placeholder = 'Description';
+savedMarkers.forEach(({ lat, lng, title, description }) => {
+    const marker = L.marker([lat, lng]).addTo(map);
+    marker.bindPopup(`<b>${title}</b><br>${description}`);
+});
 
-    var button = L.DomUtil.create('button', '', container);
-    button.innerHTML = 'Add Pin';
-
-    L.DomEvent.on(button, 'click', function() {
-        var title = inputTitle.value;
-        var desc = inputDesc.value;
-        if (title && desc) {
-            L.marker(latlng).addTo(map)
-                .bindPopup('<b>' + title + '</b><br>' + desc)
-                .openPopup();
-            map.closePopup();
-        }
-    });
-
-    return container;
-}
-
-// Add click event to add new pins
+// Add new marker on map click
 map.on('click', function(e) {
-    var popup = L.popup()
-        .setLatLng(e.latlng)
-        .setContent(createPopupForm(e.latlng))
-        .openOn(map);
+    const title = prompt("Enter title for this location:");
+    if (!title) return;
+    const description = prompt("Enter description:");
+    if (!description) return;
+
+    const marker = L.marker(e.latlng).addTo(map);
+    marker.bindPopup(`<b>${title}</b><br>${description}`);
+
+    // Save marker to localStorage
+    savedMarkers.push({
+        lat: e.latlng.lat,
+        lng: e.latlng.lng,
+        title,
+        description
+    });
+    localStorage.setItem('markers', JSON.stringify(savedMarkers));
 });
